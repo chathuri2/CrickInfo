41b56c000aa314c248c5826475c784be01fe7a21
tashima39 <151958662+tashima39@users.noreply.github.com>
2025-07-30 17:12:39 +0530

    UI
    Added the folders "squad-selector"  and
    "statistics"

---
+++ client/app/squad-selector/page.tsx
@@ -0,0 +1,5 @@
+import { SquadSelector } from "../../components/squad-selector"
+
+export default function SquadSelectorPage() {
+  return <SquadSelector />
+}
---
+++ client/app/statistics/page.tsx
@@ -0,0 +1,5 @@
+import { PlayerStatistics } from "../../components/player-statistics"
+
+export default function StatisticsPage() {
+  return <PlayerStatistics />
+}
